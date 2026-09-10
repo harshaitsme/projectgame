82387a36ee8121933b4543a3c3d18c64c07a242b
Kushan Randika Herath <k14922160@gmail.com>
2026-09-10 06:45:44 -1100

    render background and foreground layers to our current screen.

--- core/src/main/kotlin/io/github/shootgame/system/RenderSystem.kt
+++ core/src/main/kotlin/io/github/shootgame/system/RenderSystem.kt
@@ -1,7 +1,10 @@
 package io.github.shootgame.system
 
 import com.artemis.annotations.All
+import com.badlogic.gdx.graphics.OrthographicCamera
 import com.badlogic.gdx.maps.tiled.TiledMapTileLayer
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer
+import com.badlogic.gdx.maps.tiled.tiles.AnimatedTiledMapTile
 import com.badlogic.gdx.scenes.scene2d.Event
 import com.badlogic.gdx.scenes.scene2d.EventListener
 import com.badlogic.gdx.scenes.scene2d.Stage
@@ -9,6 +12,7 @@ import com.github.quillraven.fleks.*
 import com.github.quillraven.fleks.collection.compareEntity
 import io.github.shootgame.component.ImageComponent
 import io.github.shootgame.event.MapChangeEvent
+import ktx.graphics.use
 import ktx.tiled.forEachLayer
 
 
@@ -22,16 +26,40 @@ class RenderSystem(
     comparator = compareEntity { e1, e2 -> imageCmps[e1].compareTo(imageCmps[e2]) }
 ){
 
+
+
     private val bgdLayers = mutableListOf<TiledMapTileLayer>()
     private val fgdLayers = mutableListOf<TiledMapTileLayer>()
+
+    private val mapRenderer = OrthogonalTiledMapRenderer(null,1/32f,stage.batch)
+    private val orthoCam = stage.camera as OrthographicCamera
 
     override fun onTick() {
         super.onTick()
 
         with(stage){
             viewport.apply()
+
+
+            // animation tiled handling part Note: if we create some animated tile stuff that the reason for call this fun
+            AnimatedTiledMapTile.updateAnimationBaseTime()
+            mapRenderer.setView(orthoCam)
+
+
+            if(bgdLayers.isNotEmpty()){
+                stage.batch.use(orthoCam.combined){
+
+                    bgdLayers.forEach { mapRenderer.renderTileLayer(it) }
+                }
+            }
             act(deltaTime)
             draw()
+
+            if(fgdLayers.isNotEmpty()){
+                stage.batch.use(orthoCam.combined){
+                    fgdLayers.forEach { mapRenderer.renderTileLayer(it) }
+                }
+            }
         }
     }
 
