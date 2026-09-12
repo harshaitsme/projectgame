d843e255ec2b656bbf7616c42365ef0f00d1b9c7
Kushan Randika Herath <k14922160@gmail.com>
2026-09-11 23:08:04 -1100

    Updated size function in EntitySpawnSystem.kt, Changed findRegion to findRegions to get the Array of frames , Correctly resolves to AtlasRegion

--- core/src/main/kotlin/io/github/shootgame/component/EntitySpawnSystem.kt
+++ core/src/main/kotlin/io/github/shootgame/component/EntitySpawnSystem.kt
@@ -1,5 +1,7 @@
 package io.github.shootgame.component
 
+import com.badlogic.gdx.graphics.g2d.TextureAtlas
+import com.badlogic.gdx.math.Vector2
 import com.badlogic.gdx.scenes.scene2d.Event
 import com.badlogic.gdx.scenes.scene2d.EventListener
 import com.badlogic.gdx.scenes.scene2d.ui.Image
@@ -11,6 +13,7 @@ import com.github.quillraven.fleks.IteratingSystem
 import io.github.shootgame.Main.Companion.UNIT_SCALE
 import io.github.shootgame.event.MapChangeEvent
 import ktx.app.gdxError
+import ktx.math.vec2
 import ktx.tiled.layer
 import ktx.tiled.type
 import ktx.tiled.x
@@ -18,11 +21,12 @@ import ktx.tiled.y
 
 @AllOf([SpawnComponent::class])
 class EntitySpawnSystem(
-
+    private val atlas: TextureAtlas,
     private val spawnCmps: ComponentMapper<SpawnComponent>,
 ) : EventListener, IteratingSystem() {
 
     private val cachedCfgs = mutableMapOf<String, SpawnCfg>()
+    private val cachedSizes = mutableMapOf<AnimationModel, Vector2>()
 
     override fun onTickEntity(entity: Entity) {
         val spawnCmp = spawnCmps[entity]
@@ -31,8 +35,9 @@ class EntitySpawnSystem(
         world.entity {
             add<ImageComponent> {
                 image = Image().apply {
+                    val size = size(cfg.model)
                     setPosition(spawnCmp.location.x, spawnCmp.location.y)
-                    setSize(4f,4f)
+                    setSize(size.x, size.y)
                     setScaling(Scaling.fill)
                 }
             }
@@ -48,6 +53,16 @@ class EntitySpawnSystem(
             "Player" -> SpawnCfg(AnimationModel.PLAYER)
             else -> gdxError("Type $type has no SpawnCfg setup")
         }
+    }
+
+    private fun size(model: AnimationModel): Vector2 = cachedSizes.getOrPut(model){
+        val regions = atlas.findRegions("${model.atlasKey}/${AnimationType.IDLE.atlasKey}")
+        if(regions.isEmpty){
+            gdxError("There are no regions for the idle animation of model $model")
+        }
+
+        val firstFrame = regions.first()
+        vec2(firstFrame.originalWidth * UNIT_SCALE, firstFrame.originalHeight * UNIT_SCALE)
     }
 
     override fun handle(event: Event?): Boolean {
