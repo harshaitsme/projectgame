b3b82ee77b7ec52a52923f52e748137904e5c212
Kushan20070126 <kushanrandikaherath@outlook.com>
2026-09-17 23:30:16 +0530

    Changes: • GameScreen.kt: Added uiStage and an InputMultiplexer so both layers can receive touch events correctly. • UiSystem.kt: Updated to add the D-pad to the uiStage instead of the game world. Buttons are now sized in pixels (100px) for better mobile scaling. • RenderSystem.kt: Updated to render the game world first, and then overlay the static uiStage on top.

--- core/src/main/kotlin/io/github/shootgame/screen/GameScreen.kt
+++ core/src/main/kotlin/io/github/shootgame/screen/GameScreen.kt
@@ -1,5 +1,7 @@
 package io.github.shootgame.screen
 
+import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.InputMultiplexer
 import com.badlogic.gdx.graphics.g2d.Batch
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
 import com.badlogic.gdx.graphics.g2d.TextureAtlas
@@ -8,14 +10,21 @@ import com.badlogic.gdx.maps.tiled.TmxMapLoader
 import com.badlogic.gdx.scenes.scene2d.EventListener
 import com.badlogic.gdx.scenes.scene2d.Stage
 import com.badlogic.gdx.utils.viewport.ExtendViewport
+import com.badlogic.gdx.utils.viewport.ScreenViewport
 import com.github.quillraven.fleks.World
 import io.github.shootgame.system.EntitySpawnSystem
 import io.github.shootgame.component.ImageComponent
+import io.github.shootgame.component.MoveComponent
 import io.github.shootgame.component.PhysicComponent
+import io.github.shootgame.component.PlayerComponent
 import io.github.shootgame.event.MapChangeEvent
 import io.github.shootgame.event.fire
 import io.github.shootgame.system.AnimationSystem
+import io.github.shootgame.system.MoveSystem
+import io.github.shootgame.system.PhysicSystem
+import io.github.shootgame.system.PlayerInputSystem
 import io.github.shootgame.system.RenderSystem
+import io.github.shootgame.system.UiSystem
 import ktx.app.KtxScreen
 import ktx.assets.disposeSafely
 import ktx.box2d.createWorld
@@ -27,7 +36,8 @@ import ktx.math.vec2
 class GameScreen : KtxScreen {
 
     private val spriteBatch : Batch = SpriteBatch()
-    private val stage: Stage = Stage(ExtendViewport(16f,9f));
+    private val stage: Stage = Stage(ExtendViewport(16f, 9f), spriteBatch)
+    private val uiStage: Stage = Stage(ScreenViewport(), spriteBatch)
     private val textureAtlas = TextureAtlas("graphics/PlayerObject.atlas")
 
     private var currentMap: TiledMap? = null
@@ -37,14 +47,21 @@ class GameScreen : KtxScreen {
     private val eWorld: World = World {
 
         inject(stage)
+        inject("uiStage", uiStage)
         inject(textureAtlas)
         inject(phWorld)
 
         componentListener<ImageComponent.Companion.ImageComponentListener>()
         componentListener<PhysicComponent.Companion.PhysicComponentListener>()
-            system<EntitySpawnSystem>()
-            system<AnimationSystem>()
-            system<RenderSystem>()
+        componentListener<PlayerComponent.Companion.PlayerComponentListener>()
+
+        system<UiSystem>()
+        system<PlayerInputSystem>()
+        system<MoveSystem>()
+        system<PhysicSystem>()
+        system<AnimationSystem>()
+        system<RenderSystem>()
+        system<EntitySpawnSystem>()
     }
 
   // ==================================================================================
@@ -54,6 +71,7 @@ class GameScreen : KtxScreen {
 
     override fun show() {
        log.debug { "GameScreen get shown" }
+        Gdx.input.inputProcessor = InputMultiplexer(uiStage, stage)
 
         /*We laod the map and fire this trigger event and hold the map event.handler*/
         eWorld.systems.forEach { system ->
@@ -68,7 +86,8 @@ class GameScreen : KtxScreen {
 
     // ==================================================================================
     override fun resize(width: Int, height: Int) {
-        stage.viewport.update(width,height,true)
+        stage.viewport.update(width, height, true)
+        uiStage.viewport.update(width, height, true)
     }
 
     override fun render(delta: Float) {
@@ -79,6 +98,7 @@ class GameScreen : KtxScreen {
 
     override fun dispose() {
         stage.disposeSafely()
+        uiStage.disposeSafely()
         textureAtlas.disposeSafely()
         eWorld.dispose()
         currentMap?.disposeSafely()
